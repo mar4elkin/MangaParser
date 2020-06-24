de9c220369b112a5c6115a15ad6289de4d0ec95b
Mark Tamarov <googlcraftlol@gmail.com>
2020-06-24 19:05:50 +0300

    feat(maven): working on maven support

--- src/com/mar4elkin/Main.java
+++ src/com/mar4elkin/Main.java
@@ -1,13 +1,16 @@
 package com.mar4elkin;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Main {
 
     public static void main(String[] args) {
         MangaChan mangaChan = new MangaChan();
-        JSONArray k = mangaChan.getImageSet("/online/469187-outlanders_v8_ch34.html");
+        JSONArray k = mangaChan.getTags();
         System.out.println(k);
-
+        System.out.println(k.get(0));
+        JSONObject i = (JSONObject) k.get(0);
+        System.out.println(i.getString("link"));
     }
 }
